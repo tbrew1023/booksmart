c398f648bccdf3f204bdef8d45a1792ce512d7b8
Trent Brew <tbrew1023@gmail.com>
2019-10-29 06:44:18 -0500

    added icons and adjusted order of urls

--- content.js
+++ content.js
@@ -20,79 +20,150 @@ $grid.packery( 'bindUIDraggableEvents', $items );
 
 $('.item-development').click(function() {
   chrome.tabs.create({
-    url: 'https://github.com'
+    url: 'https://webflow.com/'
   });
   chrome.tabs.create({
     url: 'https://codepen.io'
   });
   chrome.tabs.create({
-    url: 'https://pinterest.com'
+    url: 'https://github.com'
   });
-  //window.open('https://github.com');
-  //window.open('https://codepen.io/');
-  //window.open('https://cdnjs.com/libraries/gsap');
-  //window.open('https://code.jquery.com/');
-  //window.open('https://flutter.dev/docs/development/ui/widgets-intro');
 });
 
 $('.item-design').click(function() {
-    window.open('https://figma.com');
-    window.open('https://coolors.co/app');
-    window.open('https://https://www.materialui.co/colors');
-    window.open('https://material.io/resources/icons/?style=baseline');
-    window.open('https://uplabs.com');
-    window.open('https://dribble.com');
-    window.open('https://www.awwwards.com/');
-    window.open('https://www.pinterest.com/');
+  chrome.tabs.create({
+    url: 'https://uplabs.com'
+  });
+  chrome.tabs.create({
+    url: 'https://dribble.com'
+  });
+  chrome.tabs.create({
+    url: 'https://www.awwwards.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://www.pinterest.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://coolors.co/app'
+  });
+  chrome.tabs.create({
+    url: 'https://www.materialui.co/colors'
+  });
+  chrome.tabs.create({
+    url: 'https://material.io/resources/icons/?style=baseline'
+  });
+  chrome.tabs.create({
+    url: 'https://figma.com'
+  });
 });
 
 $('.item-music').click(function() {
-  window.open('https://open.spotify.com');
-  window.open('https://soundcloud.com');
-  window.open('https://bandcamp.com');
-  window.open('https://ujomusic.com');
+  chrome.tabs.create({
+    url: 'https://open.spotify.com'
+  });
+  chrome.tabs.create({
+    url: 'https://www.bandcamp.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://www.ujomusic.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://soundcloud.com'
+  });
 });
 
 $('.item-entertainment').click(function() {
-  window.open('https://youtube.com');
-  window.open('https://twitch.com');
-  window.open('https://netflix.com');
-  window.open('https://hulu.com');
+  chrome.tabs.create({
+    url: 'https://twitch.com'
+  });
+  chrome.tabs.create({
+    url: 'https://www.netflix.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://www.hulu.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://youtube.com'
+  });
 });
 
 $('.item-social').click(function() {
-  window.open('https://twitter.com');
-  window.open('https://instagram.com');
-  window.open('https://facebook.com');
+  chrome.tabs.create({
+    url: 'https://instagram.com'
+  });
+  chrome.tabs.create({
+    url: 'https://www.facebook.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://twitter.com'
+  });
 });
 
 $('.item-jobs').click(function() {
-  window.open('https://depaul.joinhandshake.com/');
-  window.open('https://indeed.com');
-  window.open('https://glassdoor.com');
-  window.open('https://wayup.com');
-  window.open('https://www.google.com/search?q=user+experience&ibp=htl;jobs#fpstate=tldetail&htidocid=B-0kVG4FiJoY_hX1AAAAAA%3D%3D&htivrt=jobs');
-  window.open('https://depaul.joinhandshake.com/');
+  chrome.tabs.create({
+    url: 'https://indeed.com'
+  });
+  chrome.tabs.create({
+    url: 'https://www.glassdoor.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://www.wayup.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://www.google.com/search?q=user+experience&ibp=htl;jobs#fpstate=tldetail&htidocid=B-0kVG4FiJoY_hX1AAAAAA%3D%3D&htivrt=jobs'
+  });
+  chrome.tabs.create({
+    url: 'https://depaul.joinhandshake.com'
+  });
 });
 
 $('.item-news').click(function() {
-  window.open('https://cnn.com');
-  window.open('https://nytimes.com');
-  window.open('https://news.google.com');
-  window.open('https://wsj.com');
-  window.open('https://msnbc.com');
-  window.open('https://abcnews.go.com');
+  chrome.tabs.create({
+    url: 'https://cnn.com'
+  });
+  chrome.tabs.create({
+    url: 'https://nytimes.com'
+  });
+  chrome.tabs.create({
+    url: 'https://www.news.google.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://www.wsj.com/'
+  });
+  chrome.tabs.create({
+    url: 'http://msnbc.com'
+  });
+  chrome.tabs.create({
+    url: 'https://abcnews.go.com/'
+  });
 });
 
 $('.item-communication').click(function() {
-  window.open('https://mail.google.com');
-  window.open('https://slack.com');
-  window.open('https://discordapp.com');
-  window.open('https://messenger.com');
+  chrome.tabs.create({
+    url: 'https://slack.com'
+  });
+  chrome.tabs.create({
+    url: 'https://discordapp.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://messenger.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://mail.google.com'
+  });
 });
 
 $('.item-depaul').click(function() {
-  window.open('https://d2l.depaul.edu/d2l/home');
-  window.open('https://campusconnect.depaul.edu/psp/CSPRD92/?cmd=login');
-  window.open('https://depaul.starfishsolutions.com/starfish-ops/instructor/serviceCatalog.html?tenantId=1022#/');
+  chrome.tabs.create({
+    url: 'https://campusconnect.depaul.edu/psp/CSPRD92/?cmd=login'
+  });
+  chrome.tabs.create({
+    url: 'https://depaul.starfishsolutions.com/starfish-ops'
+  });
+  chrome.tabs.create({
+    url: 'https://depaul.joinhandshake.com/'
+  });
+  chrome.tabs.create({
+    url: 'https://d2l.depaul.edu/d2l/home'
+  });
 });
